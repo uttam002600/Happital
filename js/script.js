391ced63372a48f8ff63ec46080778dbcf2800bd
Uttam <uttam002600@gmail.com>
2023-12-18 01:18:12 +0530

    Added happital website

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings:["","Maid","Live-In Maid","Babysitter/Nanny","Elderly care","Cook"],
+    typeSpeed:100,
+    backspeed:60,
+    loop:true
+});
